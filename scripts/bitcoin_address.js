bb1c7ad5c6cfe9634e0c08d31df84a582f5c995a
jlake <jim@blueskylabs.com>
2021-11-03 11:17:19 -0700

    whoo

--- scripts/bitcoin_address.js
+++ scripts/bitcoin_address.js
@@ -3,6 +3,7 @@ const { chain, common } = require('./settings');
 const ethers = require('ethers');
 const config = require('../config.json');
 const bitcoin = require('bitcoinjs-lib');
+const cashaddr = require('cashaddrjs');
 
 const argv = process.argv.slice(2);
 const mnemonic = argv[0] || config.SIGN_MNEMONIC;
@@ -19,8 +20,8 @@ function getSegwit() {
   const path = "m/84'/0'/0'/0/0";
   const hdnode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(path);
 
-  const public_key = ethers.utils.arrayify(hdnode.publicKey);
-  console.log('public_key:', hdnode.publicKey);
+  const public_key = Buffer.from(hdnode.publicKey.slice(2), 'hex');
+  console.log('public_key:', public_key);
 
   const segwit = bitcoin.payments.p2wpkh({
     pubkey: public_key,
@@ -62,9 +63,35 @@ function getDoge() {
   });
   console.log('doge:', pkh.address);
 }
+function getBCH() {
+  const BCH = {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'q',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    pubKeyHash: 0x00,
+    scriptHash: 0x32,
+    wif: 0xb0,
+  };
+  const path = "m/44'/145'/0'/0/0";
+  const hdnode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(path);
+
+  const public_key = ethers.utils.arrayify(hdnode.publicKey);
+  console.log('bch public_key:', hdnode.publicKey);
+
+  const pkh = bitcoin.payments.p2pkh({
+    pubkey: public_key,
+    network: BCH,
+  });
+  console.log('bch:', cashaddr.encode('bitcoincash', 'P2PKH', pkh.hash));
+}
 
 ethAddr();
 console.log();
 getSegwit();
 console.log();
 getDoge();
+console.log();
+getBCH();
